509f2c9de6838f7523c3ebb884a20f0a04cde8cc
Kevin2iBi <kevin.wingi@2ibi.com>
2018-12-20 22:53:47 +0200

    Initial commit

---
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -0,0 +1,14 @@
+package com.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+
+}
+
