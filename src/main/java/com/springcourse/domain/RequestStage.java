242a25784fe07ae72545d07271ef1b6e0574ef16
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-08 02:22:30 +0200

    Create request stage respository tests

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -39,7 +39,7 @@ public class RequestStage implements Serializable {
 	@Column(columnDefinition = "text")
 	private String description;
 	
-	@Column(name = "realization_date", nullable = false)
+	@Column(name = "realization_date", nullable = false, updatable = false)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizationDate;
 	
---
+++ src/test/java/com/springcourse/repository/RequestStageRepositoryTests.java
@@ -0,0 +1,57 @@
+package com.springcourse.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.RequestState;
+
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+public class RequestStageRepositoryTests {
+	@Autowired private RequestStageRepository requestStageRepository;
+	
+	@Test
+	public void AsaveTest() {
+		User owner = new User();
+		owner.setId(1L);
+		
+		Request request = new Request();
+		request.setId(1L);
+		
+		RequestStage stage = new RequestStage(null, "Foi comprado um novo laptop de marca HD e com 16 GB de RAM", new Date(), RequestState.CLOSED, request, owner);
+	
+		RequestStage createdStage = requestStageRepository.save(stage);
+		
+		assertThat(createdStage.getId()).isEqualTo(1L);
+	}
+	
+	@Test
+	public void getByIdTest() {
+		Optional<RequestStage> result = requestStageRepository.findById(1L);
+		RequestStage stage = result.get();
+		
+		assertThat(stage.getDescription()).isEqualTo("Foi comprado um novo laptop de marca HD e com 16 GB de RAM");
+	}
+	
+	@Test
+	public void listByRequestIdTest() {
+		List<RequestStage> stages = requestStageRepository.findAll();
+		assertThat(stages.size()).isEqualTo(1);
+	}
+
+}
