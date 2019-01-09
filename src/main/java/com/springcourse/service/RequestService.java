6f39c65175bd90592d7c19afd4e35740fa9f52fd
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-09 23:30:03 +0200

    Create request service

---
+++ src/main/java/com/springcourse/service/RequestService.java
@@ -0,0 +1,46 @@
+package com.springcourse.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.enums.RequestState;
+import com.springcourse.repository.RequestRepository;
+
+@Service
+public class RequestService {
+	@Autowired private RequestRepository requestRepository;
+	
+	public Request save(Request request) {
+		request.setState(RequestState.OPEN);
+		request.setCreationDate(new Date());
+		
+		Request createdRequest = requestRepository.save(request);
+		
+		return createdRequest;
+	}
+	
+	public Request update(Request request) {
+		Request updatedRequest = requestRepository.save(request);
+		return updatedRequest;
+	}
+	
+	public Request getById(Long id) {
+		Optional<Request> result = requestRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<Request> listAll() {
+		List<Request> requests = requestRepository.findAll();
+		return requests;
+	}
+
+	public List<Request> listAllByOwnerId(Long ownerId) {
+		List<Request> requests = requestRepository.findAllByOwnerId(ownerId);
+		return requests;
+	}
+}
