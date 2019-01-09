22cb41f7b9ffa580550556662ad3f240a5f9052f
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-09 22:47:10 +0200

    Create user service

---
+++ src/main/java/com/springcourse/service/UserService.java
@@ -0,0 +1,49 @@
+package com.springcourse.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcourse.domain.User;
+import com.springcourse.repository.UserRepository;
+import com.springcourse.service.util.HashUtil;
+
+@Service
+public class UserService {
+	@Autowired private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+	
+	public User update(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+	
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> listAll() {
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+
+	public User login(String email, String password) {
+		password = HashUtil.getSecureHash(password);
+		
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+	}
+}
