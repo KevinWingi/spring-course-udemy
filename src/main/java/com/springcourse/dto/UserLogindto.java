0e390277155196767c5318b40a1f567f8a907575
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-15 22:30:09 +0200

    Create user login dto class

---
+++ src/main/java/com/springcourse/dto/UserLogindto.java
@@ -0,0 +1,14 @@
+package com.springcourse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class UserLogindto {
+	private String email;
+	private String password;
+}
