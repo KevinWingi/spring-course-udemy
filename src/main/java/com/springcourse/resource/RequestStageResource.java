c87e99fa3e4b3270a1034e6750eb03ca42ff7e1f
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-15 23:48:20 +0200

    Create request stage resource

---
+++ src/main/java/com/springcourse/resource/RequestStageResource.java
@@ -0,0 +1,33 @@
+package com.springcourse.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcourse.domain.RequestStage;
+import com.springcourse.service.RequestStageService;
+
+@RestController
+@RequestMapping(value = "request-stages")
+public class RequestStageResource {
+	@Autowired private RequestStageService stageService;
+
+	@PostMapping
+	public ResponseEntity<RequestStage> save(@RequestBody RequestStage requestStage) {
+		RequestStage createdRequestStage = stageService.save(requestStage);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdRequestStage);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<RequestStage> getById(@PathVariable(name = "id") Long id) {
+		RequestStage stage = stageService.getById(id);
+		return ResponseEntity.ok(stage);
+	}
+	
+}
