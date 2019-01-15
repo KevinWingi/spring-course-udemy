dfd6023b9b0fce8f91c8e01c9727a5d93d484cdc
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-15 23:48:45 +0200

    Add list all stages by id method

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -14,12 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
 import com.springcourse.service.RequestService;
+import com.springcourse.service.RequestStageService;
 
 @RestController
 @RequestMapping(value = "requests")
 public class RequestResource {
 	@Autowired private RequestService requestService;
+	@Autowired private RequestStageService stageService;
 	
 	@PostMapping
 	public ResponseEntity<Request> save(@RequestBody Request request) {
@@ -45,6 +48,12 @@ public class RequestResource {
 	public ResponseEntity<List<Request>> listAll() {
 		List<Request> requests = requestService.listAll();
 		return ResponseEntity.ok(requests);
+	}
+	
+	@GetMapping("/{id}/request-stages")
+	public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable(name = "id") Long id) {
+		List<RequestStage> stages = stageService.listAllByRequestId(id);
+		return ResponseEntity.ok(stages);
 	}
 
 }
