d978a5c3c492d33f2c65c9609c8d6b8768d4e7d8
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-20 19:54:58 +0200

    Add default values for page, size params

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -47,8 +47,8 @@ public class RequestResource {
 	
 	@GetMapping
 	public ResponseEntity<PageModel<Request>> listAll(
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size) {
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size) {
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<Request> pm = requestService.listAllOnLazyMode(pr);
@@ -59,8 +59,8 @@ public class RequestResource {
 	@GetMapping("/{id}/request-stages")
 	public ResponseEntity<PageModel<RequestStage>> listAllStagesById(
 			@PathVariable(name = "id") Long id,
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size) {
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size) {
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<RequestStage> pm = stageService.listAllByRequestIdOnLazyModel(id, pr);
 		return ResponseEntity.ok(pm);
--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -48,8 +48,8 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<PageModel<User>> listAll(
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size) {
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size) {
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<User> pm = userService.listAllOnLazyMode(pr);
@@ -66,8 +66,8 @@ public class UserResource {
 	@GetMapping("/{id}/requests")
 	public ResponseEntity<PageModel<Request>> listAllRequestsById(
 			@PathVariable(name = "id") Long id,
-			@RequestParam(value = "size") int size,
-			@RequestParam(value = "page") int page) {
+			@RequestParam(value = "size", defaultValue = "10") int size,
+			@RequestParam(value = "page", defaultValue = "0") int page) {
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<Request> pm = requestService.listAllByOwnerIdOnLazyModel(id, pr);
