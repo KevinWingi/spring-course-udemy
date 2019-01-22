ccfae66d2d6a00a01cb48b300b731c60a08a9240
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-23 00:27:01 +0200

    Override argument not valid method

--- src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -2,21 +2,35 @@ package com.springcourse.resource.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.springcourse.exception.NotFoundException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<ApiError> handleNotFoundException(NotFoundException ex) {
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		
+		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
 }
