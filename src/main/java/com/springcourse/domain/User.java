010e1137c4320b6981493c22e1302455c64a8910
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-23 00:25:47 +0200

    Config role updatable = false

--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -48,7 +48,7 @@ public class User implements Serializable{
 	@Column(length = 100, nullable = false)
 	private String password;
 	
-	@Column(length = 20, nullable = false)
+	@Column(length = 20, nullable = false, updatable = false)
 	@Enumerated(EnumType.STRING)
 	private Role role;
 	
