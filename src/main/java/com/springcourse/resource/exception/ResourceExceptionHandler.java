5b28b0e732480f09672db9561b95a09744726683
Kevin2iBi <kevin.wingi@2ibi.com>
2019-01-28 21:53:36 +0200

    Config to retrieve errors for invalid fields

--- src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.springcourse.resource.exception;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,9 +28,14 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		List<String> errors = new ArrayList<String>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			errors.add(error.getDefaultMessage());
+		});
 		
-		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
+		String defaultMessage = "Invalid field(s)";
+		
+		ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date(), errors);
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
