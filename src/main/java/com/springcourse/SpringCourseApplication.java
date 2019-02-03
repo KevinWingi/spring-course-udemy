25975a23ce608cfa3f5d0c04060faac57f285cfa
Kevin2iBi <kevin.wingi@2ibi.com>
2019-02-04 01:02:59 +0200

    Config deployable war file

--- src/main/java/com/springcourse/SpringCourseApplication.java
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -2,9 +2,16 @@ package com.springcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCourseApplication {
+public class SpringCourseApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCourseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
