77e9b85e4c425de42646c45ccd9d0365c928e824
KevinWingi <kevinwing8@gmail.com>
2019-06-20 20:16:01 +0200

    Create security constants

---
+++ src/main/java/com/springcourse/constant/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.springcourse.constant;
+
+public class SecurityConstants {
+	public static final int JWT_EXP_DAYS = 5;
+	public static final String API_KEY = "spring-course";
+	public static final String JWT_PROVIDER = "Bearer";
+	public static final String JWT_ROLE_KEY = "role";
+}
