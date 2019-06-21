857e6fde5bcb7b7b4251c15d12406a61d39fcd31
KevinWingi <kevinwing8@gmail.com>
2019-06-21 21:33:56 +0200

    Add invalid jwt msg constant

--- src/main/java/com/springcourse/constant/SecurityConstants.java
+++ src/main/java/com/springcourse/constant/SecurityConstants.java
@@ -5,4 +5,5 @@ public class SecurityConstants {
 	public static final String API_KEY = "spring-course";
 	public static final String JWT_PROVIDER = "Bearer";
 	public static final String JWT_ROLE_KEY = "role";
+	public static final String JWT_INVALID_MSG = "Invalid Jwt token";
 }
