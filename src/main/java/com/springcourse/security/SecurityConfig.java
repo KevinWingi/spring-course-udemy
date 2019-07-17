88939bae30bfc86e0a0aa3e405557d919f98b849
KevinWingi <kevinwing8@gmail.com>
2019-07-17 20:50:51 +0200

    Enable prePost annotation

--- src/main/java/com/springcourse/security/SecurityConfig.java
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -18,7 +18,7 @@ import com.springcourse.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
