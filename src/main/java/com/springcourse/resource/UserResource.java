77d760a880110e78479aca08a97ace4f8b1d9edb
KevinWingi <kevinwing8@gmail.com>
2019-07-17 20:51:37 +0200

    Add access control on update user method

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -31,6 +32,7 @@ import com.springcourse.dto.UserUpdateRoledto;
 import com.springcourse.dto.UserUpdatedto;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
+import com.springcourse.security.AccessManager;
 import com.springcourse.security.JwtManager;
 import com.springcourse.service.RequestService;
 import com.springcourse.service.UserService;
@@ -42,6 +44,7 @@ public class UserResource {
 	@Autowired private RequestService requestService;
 	@Autowired private AuthenticationManager authManager;
 	@Autowired private JwtManager jwtManager;
+	@Autowired private AccessManager accessManager;
 	
 	@Secured({ "ROLE_ADMINISTRATOR" })
 	@PostMapping
@@ -51,6 +54,7 @@ public class UserResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
 	}
 	
+	@PreAuthorize("@accessManager.isOwner(#id)")
 	@PutMapping("/{id}")
 	public ResponseEntity<User> update(@PathVariable(name = "id") Long id, @RequestBody @Valid UserUpdatedto userdto) {
 		User user = userdto.transformToUser();
